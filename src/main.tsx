99fd33891aa2ea5ddfca19c9c17215d2961790b9
Вячеслав Саморуков <buzapta@yandex.ru>
2025-10-25 18:50:31 +0300

    step6 fix browserouter problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from '@/components/app/app';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './services/store';
 
 createRoot(document.getElementById('root')!).render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 );
